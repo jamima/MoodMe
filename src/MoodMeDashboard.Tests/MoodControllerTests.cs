338450716ba6696715ad2b3eea7f6b9e88030c31
Jaakko Mattila <jaakko.m.mattila@gmail.com>
2024-08-29 13:33:50 +0300

    Set up client and web app projects in Visual Studio and made sure they run locally

---
+++ src/MoodMeDashboard.Tests/MoodControllerTests.cs
@@ -0,0 +1,19 @@
+namespace MoodMeDashboard.Tests
+{
+    /// <summary>
+    /// Unit tests for controllers
+    /// </summary>
+    public class MoodControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ src/MoodMeDashboard.Tests/MoodServiceTests.cs
@@ -0,0 +1,19 @@
+namespace MoodMeDashboard.Tests
+{
+    /// <summary>
+    /// Unit tests for services
+    /// </summary>
+    public class MoodServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
